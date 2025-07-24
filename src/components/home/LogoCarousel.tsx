6b2b289df9ed7a9e26c80e8a883d3671d2b89a1c
Carter Vice <Cosurvivalkid@gmail.com>
2025-07-24 02:41:34 +0000

    just try this. make the logos 50% smaller

--- src/components/home/LogoCarousel.tsx
+++ src/components/home/LogoCarousel.tsx
@@ -8,17 +8,17 @@ export function LogoCarousel() {
   const logos = Array(15).fill("/images/matblklogo.png");
 
   return (
-    <div className="w-full h-[280px] overflow-x-hidden py-4 bg-transparent relative flex items-center">
+    <div className="w-full h-[140px] overflow-x-hidden py-4 bg-transparent relative flex items-center">
        <div className="absolute inset-y-0 left-0 w-16 bg-gradient-to-r from-background to-transparent z-10"></div>
        <div className="absolute inset-y-0 right-0 w-16 bg-gradient-to-l from-background to-transparent z-10"></div>
       <div className="flex animate-scroll">
         {[...logos, ...logos].map((src, index) => (
-          <div key={index} className="flex-shrink-0 mx-6" style={{ width: '280px' }}>
+          <div key={index} className="flex-shrink-0 mx-6" style={{ width: '140px' }}>
             <Image
               src={src}
               alt="MAT BLK Logo"
-              width={280}
-              height={280}
+              width={140}
+              height={140}
               className="object-contain"
             />
           </div>
