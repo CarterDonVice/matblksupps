77274c00cd9aa7f343553d9c2a473225baaf92ab
Carter Vice <Cosurvivalkid@gmail.com>
2025-07-31 17:08:22 +0000

    make the bakround carosuel move a little faster

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -118,7 +118,7 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
         'fade-in-up': 'fade-in-up 0.6s ease-out forwards',
         'scroll': 'scroll 40s linear infinite',
-        'scroll-slow': 'scroll-slow 120s linear infinite',
+        'scroll-slow': 'scroll-slow 80s linear infinite',
         'shimmer': 'shimmer 8s linear infinite',
       },
     },
