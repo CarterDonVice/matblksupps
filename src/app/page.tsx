6c4e218f4dd063e3b7ea6f04e0abdc095a0ec52f
Carter Vice <Cosurvivalkid@gmail.com>
2025-09-19 21:19:43 +0000

    how about this
    <!-- /public/icons/flex-arm.svg -->
    <svg xmlns="http://

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,23 +8,23 @@ import { ProductIngredientsSection } from '@/components/home/ProductIngredientsS
 import { Layers, Fuel, FlaskConical } from 'lucide-react';
 
 const DifferentiatorFlexIcon = () => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-10 w-10 text-primary"
-    >
-      <path d="M3 21h3l3.5-6h3l3.5 6h3" />
-      <path d="M19 15c-1.6-1.6-1.6-4.4 0-6" />
-      <path d="M12 15h2" />
-      <path d="M12 5a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2Z" />
-    </svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="h-10 w-10 text-primary"
+    aria-hidden="true"
+  >
+    <path d="M14 5h3a2 2 0 0 1 2 2v2h-5V5z"/>
+    <path d="M14 7h5"/>
+    <path d="M14 9v3c0 1.1-.9 2-2 2h-1"/>
+    <path d="M11 14c-3 0-5 2-5 4s1.8 4 4 4h4c3 0 5-2.2 5-5 0-1.7-1.3-3-3-3"/>
+    <path d="M9.5 15.5c.9-.7 1.9-1 3.5-1"/>
+  </svg>
 );
 
 
